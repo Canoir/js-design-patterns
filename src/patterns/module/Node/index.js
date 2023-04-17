e3881488e77133cf588cd7b8189e414f34cac8e9
work.vahedi <work.vahedi>
2023-04-17 13:02:12 +0330

    feat: add module pattern node example

---
+++ src/patterns/module/Node/index.js
@@ -0,0 +1,5 @@
+import { minus, plus } from "./math";
+
+console.log("plus:", plus(2, 4));
+
+console.log("minus:", minus(5, 3));
---
+++ src/patterns/module/Node/math.js
@@ -0,0 +1,7 @@
+export function plus(numberA, numberB){
+    return numberA + numberB;
+}
+
+export function minus(numberA, numberB){
+    return numberA - numberB;
+}
